647db6c2ebb98e138b294af9b28d2652dde29204
Lucas Aguiar <vintesetelucas@gmail.com>
2024-03-25 15:41:00 -0300

    feat: new carousel component

---
+++ src/components/home/home-carousel.jsx
@@ -0,0 +1,89 @@
+import Icons from '../icons'
+
+import '../../styles/components/pages/home/home-carousel.sass'
+
+const HomeCarousel = () => {
+    return (
+        <section>
+            <div className="container-carousel">
+
+                <div className="heading">
+                    <h3>Linguagens em minha</h3>
+                    <h1>Tech Stack</h1>
+                </div>
+
+                <div className="languages-sliders">
+
+                    <div className="languages-slider">
+                        <div className="languages back">
+                            <Icons src="MySQL" name="MySQL" w="88.8" h="88.8" strong="y" />
+                            <Icons src="PostgreSQL" name="PostgreSQL" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Java" name="Java" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Spring" name="Spring" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Quarkus" name="Quarkus" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Docker" name="Docker" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Python" name="Python" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Anaconda" name="Anaconda" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Node" name="Node.js" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Puppeteer" name="Puppeteer" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Express" name="Express.js" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Railway" name="Railway" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Render" name="Render" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Git" name="Git" w="88.8" h="88.8" strong="n" />
+                        </div>
+                        <div className="languages back">
+                            <Icons src="MySQL" name="MySQL" w="88.8" h="88.8" strong="y" />
+                            <Icons src="PostgreSQL" name="PostgreSQL" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Java" name="Java" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Spring" name="Spring" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Quarkus" name="Quarkus" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Docker" name="Docker" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Python" name="Python" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Anaconda" name="Anaconda" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Node" name="Node.js" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Puppeteer" name="Puppeteer" w="88.8" h="88.8" strong="y" />
+                            <Icons src="Express" name="Express.js" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Railway" name="Railway" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Render" name="Render" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Git" name="Git" w="88.8" h="88.8" strong="n" />
+                        </div>
+
+                    </div>
+                    <div className="languages-slider">
+                        <div className="languages front">
+                            <Icons src="Sass" name="Sass" w="88.8" h="88.8" strong="y" />
+                            <Icons src="JavaScript" name="JavaScript" w="88.8" h="88.8" strong="y" />
+                            <Icons src="React" name="React" w="88.8" h="88.8" strong="y" />
+                            <Icons src="TypeScript" name="TypeScript" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Angular" name="Angular" w="88.8" h="88.8" strong="n" />
+                        </div>
+                        <div className="languages front">
+                            <Icons src="Sass" name="Sass" w="88.8" h="88.8" strong="y" />
+                            <Icons src="JavaScript" name="JavaScript" w="88.8" h="88.8" strong="y" />
+                            <Icons src="React" name="React" w="88.8" h="88.8" strong="y" />
+                            <Icons src="TypeScript" name="TypeScript" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Angular" name="Angular" w="88.8" h="88.8" strong="n" />
+                        </div>
+                        <div className="languages front">
+                            <Icons src="Sass" name="Sass" w="88.8" h="88.8" strong="y" />
+                            <Icons src="JavaScript" name="JavaScript" w="88.8" h="88.8" strong="y" />
+                            <Icons src="React" name="React" w="88.8" h="88.8" strong="y" />
+                            <Icons src="TypeScript" name="TypeScript" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Angular" name="Angular" w="88.8" h="88.8" strong="n" />
+                        </div>
+                        <div className="languages front">
+                            <Icons src="Sass" name="Sass" w="88.8" h="88.8" strong="y" />
+                            <Icons src="JavaScript" name="JavaScript" w="88.8" h="88.8" strong="y" />
+                            <Icons src="React" name="React" w="88.8" h="88.8" strong="y" />
+                            <Icons src="TypeScript" name="TypeScript" w="88.8" h="88.8" strong="n" />
+                            <Icons src="Angular" name="Angular" w="88.8" h="88.8" strong="n" />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HomeCarousel

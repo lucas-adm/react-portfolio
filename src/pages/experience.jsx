d5ee3166cc69834fdadf49525836ee3e97959727
Lucas Aguiar <vintesetelucas@gmail.com>
2024-06-25 16:02:21 -0300

    🧹 cleanup: remove irrelevant projects

--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -54,41 +54,6 @@ const Experience = () => {
                     </div>
 
                     <div className="project">
-                        <h3>Movies Lib</h3>
-                        <a href="https://react-vite-imdb.onrender.com" target="_blank"><img src="imgs/projects/MoviesLib.png" alt="Imagem do site Movies Lib" /></a>
-                        <p>Aplicação feita com React+Vite utilizando a RESTful API de IMDb para fim de estudo</p>
-                        <div className="container-languages">
-                            <span>Tecnologia utilizada</span>
-                            <div className="language">
-                                <Icons src="JavaScript" name="JavaScript" w="50" h="50" />
-                                <Icons src="React" name="React" w="50" h="50" />
-                            </div>
-                        </div>
-                        <div className="buttons">
-                            <a href="https://react-vite-imdb.onrender.com" target="_blank"><button>Site</button></a>
-                            <a href="https://github.com/lucas-adm/react-vite-imdb" target="_blank"><button>Repositório</button></a>
-                        </div>
-                    </div>
-
-                    <div className="project">
-                        <h3>Blog</h3>
-                        <a href="https://angular-blog-2mq9.onrender.com/" target="_blank"><img src="imgs/projects/Blog.png" alt="Imagem do Blog feito com Angular" /></a>
-                        <p>Blog responsivo feito com Angular e TypeScript com carregamento dinâmico geral e individual para cada artigo vindo de um DB artificial.</p>
-                        <div className="container-languages">
-                            <span>Tecnologia utilizada</span>
-                            <div className="language">
-                                <Icons src="JavaScript" name="JavaScript" w="50" h="50" />
-                                <Icons src="Angular" name="Angular" w="50" h="50" />
-                                <Icons src="TypeScript" name="TypeScript" w="50" h="50" />
-                            </div>
-                        </div>
-                        <div className="buttons">
-                            <a href="https://angular-blog-2mq9.onrender.com/" target="_blank"><button>Site</button></a>
-                            <a href="https://github.com/lucas-adm/dio-challenge-angular-blog" target="_blank"><button>Repositório</button></a>
-                        </div>
-                    </div>
-
-                    <div className="project">
                         <h3>Quiz</h3>
                         <a href="https://dio-angular-quiz.onrender.com/" target="_blank"><img src="imgs/projects/Quiz.png" alt="Imagem do Quiz feito com Angular" /></a>
                         <p>Réplica do quiz do BuzzFeed feita utilizando Angular e TypeScript</p>
@@ -107,38 +72,20 @@ const Experience = () => {
                     </div>
 
                     <div className="project">
-                        <h3>Hotel Alura</h3>
-                        <a href="https://one-challenge-hotel.onrender.com" target="_blank"><img src="imgs/projects/Hotel.png" alt="Imagem do Hotel Alura" /></a>
-                        <p>Aplicação Full Stack para o cadastro de reservas e hóspedes em um hotel utilizando Spring Boot 3</p>
-                        <div className="container-languages">
-                            <span>Tecnologia utilizada</span>
-                            <div className="language">
-                                <Icons src="PostgreSQL" name="PostgreSQL" w="50" h="50" />
-                                <Icons src="Java" name="Java" w="50" h="50" />
-                                <Icons src="Spring" name="Spring" w="50" h="50" />
-                                <Icons src="JavaScript" name="JavaScript" w="50" h="50" />
-                                <Icons src="Docker" name="Docker" w="50" h="50" />
-                            </div>
-                        </div>
-                        <div className="buttons">
-                            <a href="https://one-challenge-hotel.onrender.com" target="_blank"><button>Site</button></a>
-                            <a href="https://github.com/lucas-adm/one-challenge-springboot-hotel" target="_blank"><button>Repositório</button></a>
-                        </div>
-                    </div>
-
-                    <div className="project">
-                        <h3>Pokédex</h3>
-                        <a href="https://lucas-adm.github.io/dio-challenge-javascript-pokedex/" target="_blank"><img src="imgs/projects/Pokedex.png" alt="Imagem do Pokémon Zangoose" /></a>
-                        <p>Aplicação feita em Vanilla JS consumindo a RESTful API de PokéAPI</p>
+                        <h3>Blog</h3>
+                        <a href="https://angular-blog-2mq9.onrender.com/" target="_blank"><img src="imgs/projects/Blog.png" alt="Imagem do Blog feito com Angular" /></a>
+                        <p>Blog responsivo feito com Angular e TypeScript com carregamento dinâmico geral e individual para cada artigo vindo de um DB artificial.</p>
                         <div className="container-languages">
                             <span>Tecnologia utilizada</span>
                             <div className="language">
                                 <Icons src="JavaScript" name="JavaScript" w="50" h="50" />
+                                <Icons src="Angular" name="Angular" w="50" h="50" />
+                                <Icons src="TypeScript" name="TypeScript" w="50" h="50" />
                             </div>
                         </div>
                         <div className="buttons">
-                            <a href="https://lucas-adm.github.io/dio-challenge-javascript-pokedex/" target="_blank"><button>Site</button></a>
-                            <a href="https://github.com/lucas-adm/dio-challenge-javascript-pokedex" target="_blank"><button>Repositório</button></a>
+                            <a href="https://angular-blog-2mq9.onrender.com/" target="_blank"><button>Site</button></a>
+                            <a href="https://github.com/lucas-adm/dio-challenge-angular-blog" target="_blank"><button>Repositório</button></a>
                         </div>
                     </div>
 
